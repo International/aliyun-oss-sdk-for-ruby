4689705731d2c24b6bc86852644e9a6c2a037756
George Opritescu <ssscripting@gmail.com>
2015-09-15 14:05:39 +0200

    Removed BETA version

--- lib/aliyun/oss/version.rb
+++ lib/aliyun/oss/version.rb
@@ -6,9 +6,9 @@ module Aliyun
       MINOR    = '7'
       TINY     = '0'
       #BETA     = nil # Time.now.to_i.to_s
-      BETA     = Time.now.to_i.to_s
+      # BETA     = Time.now.to_i.to_s
     end
     
-    Version = [VERSION::MAJOR, VERSION::MINOR, VERSION::TINY, VERSION::BETA].compact * '.'
+    Version = [VERSION::MAJOR, VERSION::MINOR, VERSION::TINY].compact * '.'
   end
 end
